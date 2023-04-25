3b57c0653c656d193749eaf0dc8a694b4f382401
akthrms <akt.hrms@gmail.com>
2023-04-25 09:08:31 +0900

    feat: vocab1を追加

--- ch01/vocab1/app/Main.hs
+++ ch01/vocab1/app/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import Data.Char (isLetter)
+import Data.List (group, sort)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.Environment (getArgs)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  [filename] <- getArgs
+  text <- TIO.readFile filename
+
+  let words = T.words text
+  let words' = map (T.dropAround $ not . isLetter) words
+  let cleanedWords = filter (not . T.null) words'
+  let uniqueWords = map head $ group $ sort $ map T.toCaseFold cleanedWords
+
+  TIO.putStrLn $ T.unwords uniqueWords
+  print $ length uniqueWords
